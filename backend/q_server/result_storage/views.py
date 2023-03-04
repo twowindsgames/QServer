052b08f1e068f1429f68ce0b2ce50c72f743bd57
twowindsgames <twowindsgames@gmail.com>
2023-03-04 18:52:22 +0300

    sum

--- backend/q_server/result_storage/views.py
+++ backend/q_server/result_storage/views.py
@@ -10,7 +10,7 @@ from .serializers import *
 class ResultsListView(APIView):
     def get(self, request):
         #results_data = Result.objects.all()
-        results_data = Result.objects.order_by(F('sum') * F('percent'))
+        results_data = Result.objects.order_by(F('sum') * F('percent'))[0:10]
         serializer = ResultsSerializer(results_data,  many=True)
         return Response(serializer.data)
 
